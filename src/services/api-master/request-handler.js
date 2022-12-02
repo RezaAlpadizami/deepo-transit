b30229213bac02178d77cee130b4ac7f740c8583
Reza Alpadizami <alpdizami94@gmail.com>
2022-12-02 11:34:55 +0700

    switch integrationAPI storage, warehouse, category

--- src/services/api-master/request-handler.js
+++ src/services/api-master/request-handler.js
@@ -2,7 +2,7 @@ import { create } from 'apisauce';
 import LocalStorage from 'local-storage';
 // import AuthService from '../auth/AuthService';
 
-const api = create({ baseURL: process.env.REACT_APP_LOCAL });
+const api = create({ baseURL: process.env.REACT_APP_MASTER });
 
 // api.axiosInstance.interceptors.request.use(
 //   // eslint-disable-next-line consistent-return
